35c17c3b0861f37d3e7c85e166ad7ce35f95da38
mykothai <mike.thai2303@gmail.com>
2019-12-29 14:35:03 -0800

    init commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/stockset-1.0.0.jar --spring.datasource.url=${JDBC_DATABASE_URL}
--- src/main/java/com/fsport/stockset/Controller/StocksetApplication.java
+++ src/main/java/com/fsport/stockset/Controller/StocksetApplication.java
@@ -1,4 +1,4 @@
-package com.fsport.stockset;
+package com.fsport.stockset.Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
